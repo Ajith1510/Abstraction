47c78dcb1f3b06ccf9631752c347a669df3b7f54
Ajith1510 <ajithprasanna.rr@gmail.com>
2021-08-23 12:45:06 +0530

    Folder Structure

---
+++ src/org/abst/Col.java
@@ -0,0 +1,27 @@
+package org.abst;
+
+public class Col extends College {
+
+	@Override
+	public void joshep() {
+		
+		System.out.println("www.joshep.edu.in");
+		
+	}
+
+	@Override
+	public void jamal() {
+		
+		System.out.println("www.jamal.edu.in");
+	}
+	
+	public static void main(String[] args) {
+		
+		Col c = new Col();
+		c.bishop();
+		c.joshep();
+		c.jamal();
+		c.saranathan();
+	}
+	
+}
---
+++ src/org/abst/College.java
@@ -0,0 +1,18 @@
+package org.abst;
+
+public abstract class College {
+	
+	public void bishop() {
+		
+		System.out.println("www.bhc.edu.in");
+
+	}
+	public void saranathan() {
+		
+		System.out.println("www.saranathan.edu.in");
+
+	}
+	public abstract void joshep();
+	public abstract void jamal() ;
+	
+}
